270bf8990ef0cd200925961b1923fdea4b4d636b
Samyok Nepal <nepalsamyok@gmail.com>
2019-09-02 21:10:39 -0500

    2020 version

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Samyok Nepal</h1>
+            <p>I love making things! I'd love to go to the following places: </p>
+            <ul>
+                <li>Japan</li>
+                <li>Germany</li>
+                <li>Singapore</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo;
