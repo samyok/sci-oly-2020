cd032bd875d39c52273ff3ef2e0cfbd7c61db348
Samyok Nepal <nepalsamyok@gmail.com>
2019-12-21 13:27:18 -0600

    updated with embedded doc

---
+++ docs/precache-manifest.68b2f2611a2a36adb4d96f89d78f839f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5badf73c73515dd59c68f17b4d507ab1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06fe2c2a2bb2f6f0c08f",
+    "url": "/static/js/2.8b59e765.chunk.js"
+  },
+  {
+    "revision": "925f5bc35d37f2532cb3",
+    "url": "/static/js/main.3fb5dda0.chunk.js"
+  },
+  {
+    "revision": "e4d1daf349c41e131e27",
+    "url": "/static/js/runtime~main.d5f589b4.js"
+  }
+]);
